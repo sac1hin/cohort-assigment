d669530af5906ffadc18b632b031bd009e833a4d
SAC6969 <sachinvermaa1234@gmail.com>
2024-01-18 21:24:43 +0530

    1st

---
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useMemo, useState } from "react";
+
+// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
+// Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
+
+export function Assignment1() {
+    const [input, setInput] = useState(0);
+    // Your solution starts here
+    let expensiveValue = useMemo(() => {
+        let expensiveValue = 1;
+        for (let i = 1; i <= Math.abs(input); i++) {
+            expensiveValue = expensiveValue * i;
+        }
+
+        if (input < 0) {
+            expensiveValue *= -1;
+        }
+        return expensiveValue;
+    }, [input]);
+    // Your solution ends here
+
+    return (
+        <div>
+            <input
+                type="number"
+                value={input}
+                onChange={(e) => setInput(Number(e.target.value))}
+            />
+            <p>Calculated Value: {expensiveValue}</p>
+        </div>
+    );
+}
---
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useCallback } from 'react';
+import { useRef } from 'react';
+
+// Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
+
+let numberOfTimesRender = 0;
+export function Assignment2() {
+
+    const [count, setCount] = useState(0);
+
+    const handleReRender = () => {
+        // Update state to force re-render
+        setCount(count => count + 1);
+    };
+
+    numberOfTimesRender = numberOfTimesRender + 1; 
+
+    return (
+        <div>
+            <p>This component has rendered {numberOfTimesRender} times.</p>
+            <button onClick={handleReRender}>Force Re-render</button>
+        </div>
+    );
+};
